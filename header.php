7d03e78d36f12d5cbcea987be7978c8e9613bc5b
Anthony CHAMBON <acdf-ac.cdf@hotmail.fr>
2022-12-15 16:54:35 +0100

    mise en place des fichiers

---
+++ header.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="./style.css">
+<div id="entete">
+    <div id="logo">
+        <img src="./images/logo.jpg" alt="logo squatch">
+    </div>
+
+    <h2>Au jeu de paume moderne</h2>
+
+</div>
+
+<!-- <nav>
+    <ul>
+        <a href="zozor-acceuil.php">ACCEUIL</a>
+        <a href="zozor-blog.php">BLOG</a>
+        <a href="zozor-cv.php">CV</a>
+        <a href="zozor-contact.php">CONTACT</a>
+    </ul>
+</nav> -->
